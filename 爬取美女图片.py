c715917730d0312cf31008143ec1696135a6fcc0
GeekHaozai <1700118540@qq.com>
2023-07-10 14:52:04 +0800

    日更

--- 爬取美女图片.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-headers={
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36 Edg/111.0.1661.41'
-
-}
-response=requests.get('https://zhuanlan.zhihu.com/p/389477042',headers=headers)
-response.encoding='utf-8'
-print(response.text)
